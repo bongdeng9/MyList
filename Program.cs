d4ea9d9d382291696570d017591116ecdc574b6d
bongdeng9 <80138709+bongdeng9@users.noreply.github.com>
2022-07-05 17:14:08 +0900

    동적배열
    리스트의 기능중 Add RemoveAt index 기능 구현

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            board.Initialize();
+
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+
+            int lastTick = 0;
+            while (true)
+            {
+                #region 프레임관리
+                // FPS 프레임 (60 프레임) 이 루프가 1초에 몇번 업데이트 하는지 
+                int currentTick = System.Environment.TickCount;
+
+                // 만약에 경과한 시간이 1/30초 보다 작다면,
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+                #endregion
+
+                // 입력
+
+                // 로직
+
+                // 렌더링
+                Console.SetCursorPosition(0, 0);
+
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+
+                }
+            }
+
+        }
+    }
+}
